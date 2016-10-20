1c1c2dbfa6e2d1f2638899105674b2c7be34891e
Maxwell Russell <maxruss.87@gmail.com>
2016-10-20 18:23:34 -0500

    adding event listeners for buttons

--- main.js
+++ main.js
@@ -1,16 +1,57 @@
-console.log('hello');
+var battleArena = $('.battleContainer');
 
-var battleArea = $('.battleContainer');
-
-function renderBattleArea(battleArea) {
-  var statContainer = $('<ul class="statContainer"></ul>');
-  var $li = $('<li></li>');
+function renderBattleArena(battleArena, character, boss) {
+  //as there is only currently only one player all action buttons only need to provide responses for one player 
   var $actionNav = $('<nav class="characterActions"></nav>');
-  battleArea.append(statContainer.append($li));
+  var $attackButton = $('<button class="attack">Attack</button>');
+  var $defendButton = $('<button class="defend">Defend</button>');
+  var $powerButton = $('<button class="specialPower">Super Power</button>');
+
+  var renderStats(character) {
+    var statContainer = $('<ul class="statContainer"></ul>');
+    var $li = $('<li></li>');
+    var $healthBar = $('<div class="health"></div>');
+    var $currentHealth = $('<h2>current health/total health</h2>');
+    var $characterName = $('<h1>Character Name</h1>');
+    battleArena.append(statContainer.append($li.append($characterName, $healthBar.append($currentHealth))));
+
+  }
+
+  function getCharacterStats() {
+    //this will get the info for the current character and then pass the arguments in so that new info can be passed along to the stat renderer and display current into
+    renderStats(arguments);
+  }
+
+  function getBossStats() {
+    //this will get the info for the boxx and then pass the arguments in so that new info can be passed along to the stat renderer and display current boss stats
+  }
+
+  battleArena.find('.attack').on('click', function(e){
+    e.preventDefault();
+    var hit = character.attack();
+    return hit;
+  });
+
+  battleArena.find('.defend').on('click', function(e){
+    e.preventDefault();
+    character.defend();
+  });
+
+  battleArena.find('.specialPower').on('click', function(e){
+    e.preventDefault();
+    character.superPower();
+    var hit = character.specialPower();
+    return hit;
+  });
+
+  battleArena.append($actionNav.append($attackButton, $defendButton, $powerButton));
+  renderStatBox();
+
+  console.log(battleArena.find('.health'));
+
 }
 
-renderBattleArea(battleArea);
-
+renderBattleArena(battleArena);
 
 // this.attack === sword
 //
